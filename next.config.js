326ee29159f4336eb74571ecc8047342eefe25dd
koagie <kogichan.pikapuuu.17.6@gmail.com>
2022-12-19 23:00:00 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "links.papareact.com"
-    ],
-  }
 }
+
+module.exports = nextConfig
